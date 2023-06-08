bc45b032a3406ba91e090324f38abc3e009536d8
Hazim Developer <hazimdeveloper@gmail.com>
2023-06-08 11:11:11 +0800

    first commit

---
+++ assets/zeno-page-85329805386.js
@@ -0,0 +1,15 @@
+
+          function znReady(fn) {
+            if (document.readyState != 'loading'){
+              fn();
+            } else {
+              document.addEventListener('DOMContentLoaded', fn);
+            }
+          }
+
+          window.znStorage={_storage:new WeakMap,put:function(e,t,n){this._storage.has(e)||this._storage.set(e,new Map),this._storage.get(e).set(t,n)},get:function(e,t){return this._storage.get(e).get(t)},has:function(e,t){return this._storage.has(e)&&this._storage.get(e).has(t)},remove:function(e,t){var n=this._storage.get(e).delete(t);return 0===!this._storage.get(e).size&&this._storage.delete(e),n}};
+          
+          
+          Element.prototype.matches||(Element.prototype.matches=Element.prototype.msMatchesSelector||Element.prototype.webkitMatchesSelector),Element.prototype.closest||(Element.prototype.closest=function(e){var t=this;do{if(Element.prototype.matches.call(t,e))return t;t=t.parentElement||t.parentNode}while(null!==t&&1===t.nodeType);return null});
+          window.znRespondToVisibility=function(e,t){var n={root:null,threshold:.01},o=new IntersectionObserver((function(e,n){var o=e.map((function(e){return e.isIntersecting})),a=o.includes(!0);t(a)}),n);o.observe(e)};
+        ; znReady((function(){document.getElementById("znid-334201829182"),document.querySelector(".zn-container.editing"),document.getElementById("znid-908179018498"),document.querySelector(".zn-container.editing"),document.getElementById("znid-419231774505"),document.querySelector(".zn-container.editing"),document.getElementById("znid-972572121062"),document.querySelector(".zn-container.editing"),document.getElementById("znid-519086822961"),document.querySelector(".zn-container.editing"),document.getElementById("znid-085694069837"),document.querySelector(".zn-container.editing"),document.getElementById("znid-804291901127"),document.querySelector(".zn-container.editing"),document.getElementById("znid-528702031724"),document.querySelector(".zn-container.editing"),document.getElementById("znid-569703907870"),document.querySelector(".zn-container.editing"),document.getElementById("znid-180631134819"),document.querySelector(".zn-container.editing"),document.getElementById("znid-706184039652"),document.querySelector(".zn-container.editing"),document.getElementById("znid-711535406750"),document.querySelector(".zn-container.editing"),document.getElementById("znid-043075910519"),document.querySelector(".zn-container.editing"),document.getElementById("znid-271381747252"),document.querySelector(".zn-container.editing"),document.getElementById("znid-467827362756"),document.querySelector(".zn-container.editing"),document.getElementById("znid-344466071162"),document.querySelector(".zn-container.editing"),document.body.style.removeProperty("background-color"),document.querySelectorAll(".page-top, #module-content").forEach((function(e){e.style.removeProperty("background-color")})),document.querySelector(".zn-container.editing"),function(){document.querySelector(".zn-container.editing");var e=document.body;n();function n(){var n=e.querySelector(".zn-announcement-bar");n&&n.parentNode.removeChild(n)}}()}));
